6a8a31150b59e019cfc80d12d8f36ac9fb6d214d
glynk <glyn_knight@hotmail.com>
2022-10-03 14:40:20 +0100

    Trying to fix deployed images

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,30 +8,30 @@ const Projetcs = () =>{
         <div className={Styles.project_grid}>
             <div className={Styles.project_card}>
             <a href='https://github.com/GLYNKNIGHT/Job-Tracking-App-Demo' target='blank'> 
-            <img src='./Progress.png' alt='progress' />
+            <img src='./Progress.PNG' alt='progress' />
             </a>
             </div>
             <div className={Styles.project_card}>
             <a href='https://github.com/GLYNKNIGHT/Untrodden-Final-Team-Project' target='blank'> 
-                <img src='./untrodden.png' alt='untrodden' />
+                <img src='./untrodden.PNG' alt='untrodden' />
                 </a>
             </div>
             <div className={Styles.project_card}>
             <a href='https://github.com/GLYNKNIGHT/Band-Its-Week-8-Hackathon-' target='blank'>  
-                <img src='./BandIts.png' alt='Band-Its' />
+                <img src='./BandIts.PNG' alt='Band-Its' />
             </a>
             </div>
             <div className={Styles.project_card}>
             <a href='https://github.com/GLYNKNIGHT/Geta-Note--Wk9--Team-Project' target='blank'>  
-                <img src='./GetaNote.png' alt='GetaNote' />
-                </a>
+                <img src='./GetaNote.PNG' alt='GetaNote' />
+            </a>
             </div>
             <div className={Styles.project_card}>
                 <img src='./ToDo.png' alt='ToDo' />
             </div>
             <div className={Styles.project_card}>
                 
-                <img src='./Med.png' alt='Mediation' />
+                <img src='./Med.PNG' alt='Mediation' />
             </div>
         </div>
     </div>
