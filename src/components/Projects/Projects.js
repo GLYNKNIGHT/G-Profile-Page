b957f1b6027649d10d10085fd3fb69cd843bc6f4
glynk <glyn_knight@hotmail.com>
2022-10-03 14:35:08 +0100

    Updated Alt text

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,25 +13,25 @@ const Projetcs = () =>{
             </div>
             <div className={Styles.project_card}>
             <a href='https://github.com/GLYNKNIGHT/Untrodden-Final-Team-Project' target='blank'> 
-                <img src='./untrodden.png' alt='progress' />
+                <img src='./untrodden.png' alt='untrodden' />
                 </a>
             </div>
             <div className={Styles.project_card}>
             <a href='https://github.com/GLYNKNIGHT/Band-Its-Week-8-Hackathon-' target='blank'>  
-                <img src='./BandIts.png' alt='progress' />
+                <img src='./BandIts.png' alt='Band-Its' />
             </a>
             </div>
             <div className={Styles.project_card}>
             <a href='https://github.com/GLYNKNIGHT/Geta-Note--Wk9--Team-Project' target='blank'>  
-                <img src='./GetaNote.png' alt='progress' />
+                <img src='./GetaNote.png' alt='GetaNote' />
                 </a>
             </div>
             <div className={Styles.project_card}>
-                               <img src='./ToDo.png' alt='progress' />
+                <img src='./ToDo.png' alt='ToDo' />
             </div>
             <div className={Styles.project_card}>
                 
-                <img src='./Med.png' alt='progress' />
+                <img src='./Med.png' alt='Mediation' />
             </div>
         </div>
     </div>
