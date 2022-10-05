401418d29e63dee622783f3d287585c3f2f79242
glynk <glyn_knight@hotmail.com>
2022-10-05 10:01:35 +0100

    Created project card component

---
+++ src/components/Projects/ProjectCard/ProjectCard.js
@@ -0,0 +1,16 @@
+import Styles from '../Projects.module.css'
+import { useState } from 'react'
+
+const ProjectCard = ({title, url, img, description}) =>{
+
+    const [mouseEnter, setMouseEnter] = useState(false)
+
+    return  <div className={Styles.project_card} onMouseEnter={()=>setMouseEnter(true)}  onMouseLeave={()=>setMouseEnter(false)}>
+    <a href={url} target='blank'> 
+    <img src={img} alt={title} />
+    {(mouseEnter===true)?<div className={Styles.hidden}><p>{description}<br></br>Click to see Git Hub Repo</p></div>: null}
+    </a>
+    </div>
+}
+
+export default ProjectCard
---
+++ src/components/Projects/ProjectsData.js
@@ -0,0 +1,42 @@
+let ProjectData = [
+{id:1,
+    title: 'Progress',
+    ulr: 'https://github.com/GLYNKNIGHT/Job-Tracking-App-Demo',
+    img: './Progress.PNG',
+    description: 'Job application tracking App'
+},
+{id:2,
+    title: 'Untrodden',
+    ulr: 'https://github.com/GLYNKNIGHT/Untrodden-Final-Team-Project',
+    img: './untrodden.PNG',
+   description: 'Team Project - Shared places to visit'
+   },
+{id:3,
+    title: 'Band-Its',
+    ulr: 'https://github.com/GLYNKNIGHT/Band-Its-Week-8-Hackathon-',
+    img: './BandIts.PNG',
+   description: 'Band API'
+   },
+{id:4,
+    title: 'GetaNote',
+    ulr: 'https://github.com/GLYNKNIGHT/Geta-Note--Wk9--Team-Project',
+    img: './GetaNote.PNG',
+   description: 'Note sharing App'
+   },
+{id:5,
+    title: 'To Do',
+    ulr: '',
+    img: './ToDo.png',
+   description: 'To Do App'
+   },
+{id:6,
+    title: 'Mediation',
+    ulr: 'https://github.com/GLYNKNIGHT/Job-Tracking-App-Demo',
+    img: './Med.PNG' ,
+   description: 'Mediation App'
+   },
+           
+
+]
+
+export default ProjectData
